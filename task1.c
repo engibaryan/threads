5a9fe2edbbcc108d11d4df9e3fe9c4461650bc58
engibaryan <xachik.engibar@gmail.com>
2024-11-11 12:19:51 +0000

    first task

---
+++ task1.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+void* message(void* arg) {
+	int thread_id = *((int*)arg);
+	printf("Thread %d is running\n", thread_id);
+	pthread_exit(NULL);
+}
+
+
+int main() {
+        pthread_t t1;
+	pthread_t t2;
+	pthread_t t3;
+
+	const int id1 = 1;
+	const int id2 = 2;
+	const int id3 = 3;
+
+	pthread_create(&t1, NULL, message,(void*)&id1);
+	pthread_create(&t2, NULL, message,(void*)&id2);
+        pthread_create(&t3, NULL, message,(void*)&id3);	
+	
+	pthread_join(t1,NULL);
+	pthread_join(t2,NULL);
+	pthread_join(t3,NULL);
+
+	printf("All threads have completed. \n");
+       	
+	return 0;
+}
